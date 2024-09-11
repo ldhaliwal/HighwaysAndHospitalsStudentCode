9a45a350985e6e693c671f4dbca04309d0172ce1
ldhaliwal <liliana.dhaliwal@menloschool.org>
2024-09-10 22:48:30 -0700

    initial brainstorming

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date        |   Time   |                                                                                                                                                                                              Update |
+|:------------|:--------:|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| September 9 | 1:30-2pm | Started thinking about how to approach the problem, mainly brainstorming on paper. talked with classmates about different cases, specifically when the hospital cost is lower than the highway cost |
+|             |          |                                                                                                                                                                                                     |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Liliana Dhaliwal
  *
  */
 
@@ -15,6 +15,38 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+
+
+        // Easy check for if hospitals are cheaper than highways, build at hospital in each city
+        if (hospitalCost <= highwayCost){
+            return ((long) hospitalCost * n);
+        }
+
+        // Thinking about other case(s?)
+        // what if hospitals and highways cost the same? does that even matter?
+            // update from later: i think this can get grouped with the
+            // "if hospitals are cheaper one" because it doesn't matter
+
+
+        //How to approach this:
+        // weighted edges?
+            // find the cheapest path? BFS again but with weights??
+
+        // cyclying through each possibility
+            // ie. if hospital is placed at city 1, then recursive all possibilities of highways/hospitals being places
+        // seems like it would take too long and be really inefficient, but that seems like the best idea?
+
+        // or some sort of nested loops?
+        // if currentCost < cost, cost = currentCost
+            // ^^ that sort of thing to check what is the cheapest combination
+
+
+        // not sure what the algorithm is supposed to look like at all
+
+
+
+
+
         return 0;
     }
 }
