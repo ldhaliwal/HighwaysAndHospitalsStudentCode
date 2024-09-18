d098495deec6f5a1c46778baddee2b161bf41cd3
ldhaliwal <liliana.dhaliwal@menloschool.org>
2024-09-17 20:41:39 -0700

    got it working wooooo!!! need to comment still

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.ArrayList;
 
 /**
@@ -16,59 +17,42 @@ public class HighwaysAndHospitals {
      * TODO: Complete this function, cost(), to return the minimum cost to provide
      *  hospital access for all citizens in Menlo County.
      */
-    public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        System.out.println(cities[0][0]);
-        System.out.println(cities[0][1]);
 
-        long totalCost = 0;
+    public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        // Make a map (array) of size n +1 to store all root/node connections (starting at index 1, not 0 for clarity)
+        int[] root = new int[n + 1];
 
         // Easy check for if hospitals are cheaper than highways, build at hospital in each city
         if (hospitalCost <= highwayCost){
             return ((long) hospitalCost * n);
         }
 
-        // make an arraylist of arraylists
-        ArrayList<ArrayList<Integer>> clusters = new ArrayList<ArrayList<Integer>>();
+        // for each pair of nodes:
+        for (int i = 0; i< cities.length; i++){
+            int parentRoot = findRoot(cities[i][0], root);
+            int childRoot = findRoot(cities[i][1], root);
 
-        // need to find number of clusters.
-        for(int i = 0; i < cities.length; i++){
-            for(int j = 0; j < cities[0].length; j++){
-                if (!clusters.get(j).contains(cities[i][j])){
-                    //create new subcluster and add cities[i][j]
-                }
+            if (childRoot != parentRoot){
+                root[childRoot] = parentRoot;
             }
         }
 
-        for (int i = 0; i < clusters.size(); i++){
-            totalCost += ((long) hospitalCost + (highwayCost * clusters.get(i).size - 1));
+        int numClusters = 0;
+        for (int i = 0; i< root.length; i++){
+            if(root[i] == 0 && i !=0 ){
+                numClusters++;
+            }
         }
-        // for each cluster,
-            //totalCost += (long) (hospital cost + (highways cost * [numCitiesInCluster - 1]
 
+        return (((long) numClusters * hospitalCost) + ((long) (n - numClusters) *highwayCost));
 
+    }
 
-        // Thinking about other case(s?)
-        // what if hospitals and highways cost the same? does that even matter?
-            // update from later: i think this can get grouped with the
-            // "if hospitals are cheaper one" because it doesn't matter
-
-
-        //How to approach this:
-        // weighted edges?
-            // find the cheapest path? BFS again but with weights??
-
-        // cyclying through each possibility
-            // ie. if hospital is placed at city 1, then recursive all possibilities of highways/hospitals being places
-        // seems like it would take too long and be really inefficient, but that seems like the best idea?
-
-        // or some sort of nested loops?
-        // if currentCost < cost, cost = currentCost
-            // ^^ that sort of thing to check what is the cheapest combination
-
-
-        // not sure what the algorithm is supposed to look like at all
-
-
-        return 0;
+    public static int findRoot(int i, int[] root){
+        if (root[i] == 0) {
+            return i;
+        }
+        root[i] = findRoot(root[i], root); // Path compression
+        return root[i];
     }
 }
